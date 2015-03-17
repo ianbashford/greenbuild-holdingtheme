1dd893e2df989cfad6f9c91243f333e38b10969a
Ian Bashford <ian@bashford.net>
2015-03-17 20:00:22 +0000

    first commit

---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Greenbuild Landscapes - Hampshire</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet" media="all" type="text/css" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/bootstrap.min.css" type="text/css" media="screen" />
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/freelancer.css" type="text/css" media="screen" />
+
+    <!-- Custom Fonts -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-60844324-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</head>
+
+<body id="page-top" class="index">
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/img/IconPNG.png" alt="">
+                    <img class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/img/TextPNG.png" alt="">
+
+                    <div class="intro-text">
+                        <!-- <span class="name">Garden Design Build Services in Hampshire</span>  -->
+                        <hr class="star-light">
+                        <span class="skills"> Designing and building gardens throughout Hampshire and the South Coast</span>
+                        <p></p><span class="skills">07833 445 557  </span>
+                        <p></p><span class="skills"><a href="mailto:chris@greenbuildlandscapes.co.uk">  chris@greenbuildlandscapes.co.uk</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
